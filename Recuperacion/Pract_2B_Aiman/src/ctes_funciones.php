d7107fd3db573421a7f623ed5df739ac375aebd8
Aiman <ahardao1001@g.educaand.es>
2025-04-10 13:09:19 +0200

    corregir todo

--- Recuperacion/Pract_2B_Aiman/src/ctes_funciones.php
+++ Recuperacion/Pract_2B_Aiman/src/ctes_funciones.php
@@ -4,7 +4,7 @@
 define("SERVIDOR_BD", "localhost");
 define("USUARIO_BD", "jose");
 define("CLAVE_BD", "josefa");
-define("NOMBRE_BD", "bd_cv");
+define("NOMBRE_BD", "bd_libreria_exam");
 
 function LetraNIF($dni)
 {
